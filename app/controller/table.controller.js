8076f0ba9ab37cb8bdf52f9384871c51d5210d76
DaffPrabal <prabal.jain@daffodilsw.com>
2021-09-16 13:41:17 +0530

    updated code for userlist and dropdown filter

--- app/controller/table.controller.js
+++ app/controller/table.controller.js
@@ -2,23 +2,51 @@ const db = require("../models");
 const header = db.header;
 const filter = db.filter;
 const worker = db.workers;
+const user = db.user;
 
 exports.sendAllWorkersList = async (req, res) => {
-  console.log(req);
   let head;
   let filt;
   let allData;
-  // if (!req.body.class) {
-  //   res.status(400).send({ message: "Content can not be empty!" });
-  //   return;
-  // }
+  if (!req.body.class) {
+    res.status(400).send({ message: "Content can not be empty!" });
+    return;
+  }
   let start = req.body.start ? req.body.start : 0;
   let limit = req.body.limit ? req.body.limit : 10;
   let sort = req.body.sort;
+  let findFilter = req.body.filter;
   let classname = req.body.class;
   let findCondition = {};
   let sortCondition = {};
-
+  const arr = [];
+  if (req.body.filter.length) {
+    for (let i = 0; i < findFilter.length; i++) {
+      let cond = findFilter[i];
+      let conditionHeader = cond.header;
+      let conditionValue = cond.value;
+      if (conditionValue !== "") {
+        let obj = {};
+        if (conditionValue != true && conditionValue != false) {
+          let temphe = conditionHeader + ".value";
+          obj[temphe] = {
+            $regex: new RegExp(conditionValue),
+            $options: "i",
+          };
+        } else if (conditionValue == true || conditionValue == false) {
+          let temph = conditionHeader + ".value";
+          obj[temph] = conditionValue;
+        } else {
+        }
+        arr.push(obj);
+      }
+    }
+  }
+  findCondition = req.body.filter.length
+    ? {
+        $or: arr,
+      }
+    : {};
   if (sort.property) {
     let order = sort.order === "asc" ? 1 : -1;
     sortCondition[sort.property] = order;
@@ -41,13 +69,23 @@ exports.sendAllWorkersList = async (req, res) => {
     });
   }
   try {
-    allData = await worker.find(findCondition).sort(sortCondition).exec();
-    tableData = await worker
-      .find(findCondition)
-      .sort(sortCondition)
-      .skip(start)
-      .limit(limit)
-      .exec();
+    if (classname === "workerList") {
+      allData = await worker.find(findCondition).sort(sortCondition).exec();
+      tableData = await worker
+        .find(findCondition)
+        .sort(sortCondition)
+        .skip(start)
+        .limit(limit)
+        .exec();
+    } else if (classname === "userList") {
+      allData = await user.find(findCondition).sort(sortCondition).exec();
+      tableData = await user
+        .find(findCondition)
+        .sort(sortCondition)
+        .skip(start)
+        .limit(limit)
+        .exec();
+    }
   } catch (err) {
     console.log(err);
     return res.status(400).json({
